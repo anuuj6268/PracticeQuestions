6d7eed231073ab09fbd3a2319ac3b0f5d7284a04
Anuj <147028172+anuuj6268@users.noreply.github.com>
2025-10-11 21:52:06 +0530

    Time: 20 ms (12.46%), Space: 44.2 MB (13.77%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,15 @@
+import java.math.*;
+class Solution {
+    public String addStrings(String num1, String num2) {
+        BigInteger a = new BigInteger(num1);
+        BigInteger b = new BigInteger(num2);
+        BigInteger c = a.add(b);
+        String sum = c.toString();
+       return sum;
+
+
+
+
+
+    }
+}
