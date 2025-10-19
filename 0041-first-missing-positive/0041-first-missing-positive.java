992dfa6187a09dafe05db241de471b2bb2f81693
Anuj <147028172+anuuj6268@users.noreply.github.com>
2025-10-19 22:51:40 +0530

    Time: 15 ms (17.81%), Space: 56.5 MB (85.91%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 class Solution {
     public int firstMissingPositive(int[] nums) {
         Arrays.sort(nums);
-        int smallest = 1; 
+        int smallest1 = 1; 
         
         for (int num : nums) {
-            if (num == smallest) {
-                smallest++; 
+            if (num == smallest1) {
+                smallest1++; 
             }
         }
-        return smallest; 
+        return smallest1; 
     }
 }
