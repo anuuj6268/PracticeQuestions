6bccfe1c9ee7fe77cf8b7511a98499059fb627a6
Anuj <147028172+anuuj6268@users.noreply.github.com>
2025-10-23 21:45:50 +0530

    Update 1952-three-divisors.java

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isThree(int n) {
-        int count = 0;
+        int count1 = 0;
         for(int i = 1;i<=n;i++){
             if(n%i==0){
-                count++;
+                count1++;
             }
         }
-        if(count==3){
+        if(count1==3){
             return true;
         }
 return false;    }
