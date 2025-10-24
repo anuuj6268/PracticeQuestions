c5ebd13869cb3baf3b9dea41ea976a06635f3d4f
Anuj <147028172+anuuj6268@users.noreply.github.com>
2025-10-24 21:25:01 +0530

    Update 0509-fibonacci-number.java

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,14 +2,14 @@ class Solution {
     public int fib(int n) {
         int previous = 0;
         int next = 1;
-        int current = next;
+        int current1 = next;
         if(n==1) return 1;
         if(n==0) return 0; 
         for(int i = 2;i<=n;i++){
-            current = previous + next;
+            current1 = previous + next;
             previous = next;
-            next = current;
+            next = current1;
         }
-    return current;
+    return current1;
     }
 }
