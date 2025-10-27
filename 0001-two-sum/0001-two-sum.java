caa7399d13787c6db8360b26d80ac314d514c411
Anuj <147028172+anuuj6268@users.noreply.github.com>
2025-10-27 21:34:41 +0530

    Rename array variable for twoSum method

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] twoSum(int[] num, int target) {
-        int arr[]=new int[2];
+        int arr1[]=new int[2];
         for(int i = 0;i<num.length-1;i++){
             for(int j = i+1;j<num.length;j++){
                 if(num[i]+num[j]==target){
-                    arr[0]=i;
-                    arr[1]=j;
+                    arr1[0]=i;
+                    arr1[1]=j;
                 }
             }
         }
-   return arr;
+   return arr1;
    }
 }
