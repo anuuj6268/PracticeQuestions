520b42ad81fc34d5d8266d431cba33869095023e
Anuj <147028172+anuuj6268@users.noreply.github.com>
2025-11-12 19:05:44 +0530

    Time: 1 ms (28.52%), Space: 43.2 MB (6.61%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,9 @@
 class Solution {
-public static String[] wordList(String str){
-    return str.split(" ");
+public static String[] wordList(String str1){
+    return str1.split(" ");
 }
-public int lengthOfLastWord(String str) {
-String wordlist[] = wordList(str);
+public int lengthOfLastWord(String str1) {
+String wordlist[] = wordList(str1);
 String lastWords = wordlist[wordlist.length-1];
 return lastWords.length();        
     }
