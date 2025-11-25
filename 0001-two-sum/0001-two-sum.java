98e005c2c07d68b9e9d31d458f038324361fcfc9
Anuj <147028172+anuuj6268@users.noreply.github.com>
2025-11-25 19:49:58 +0530

    Time: 50 ms (7.39%), Space: 46.2 MB (31.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int[] twoSum(int[] nums, int targets) {
+    public int[] twoSum(int[] nums, int target) {
         int arr[]=new int[2];
         for(int i = 0;i<nums.length-1;i++){
             for(int j = i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==targets){
+                if(nums[i]+nums[j]==target){
                     arr[0]=i;
                     arr[1]=j;
                 }
