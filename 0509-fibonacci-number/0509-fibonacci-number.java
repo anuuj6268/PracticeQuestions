6e67e67665b3b9d0fdd1e841f59fe5b3b3a5d9d8
Anuj <147028172+anuuj6268@users.noreply.github.com>
2025-12-07 22:01:29 +0530

    Time: 0 ms (100%), Space: 41.6 MB (53.36%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int fib(int num) {
+    public int fib(int number) {
         int previous = 0;
         int next = 1;
         int current = next;
-        if(num==1) return 1;
-        if(num==0) return 0; 
-        for(int i = 2;i<=num;i++){
+        if(number==1) return 1;
+        if(number==0) return 0; 
+        for(int i = 2;i<=number;i++){
             current = previous + next;
             previous = next;
             next = current;
