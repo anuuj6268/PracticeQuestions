28e43ac7e799a10aef55a6deb0b4d9ae3b83b631
Anuj <147028172+anuuj6268@users.noreply.github.com>
2025-12-27 13:35:25 +0530

    Time: 24 ms (23.19%), Space: 76.4 MB (90.91%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,8 +1,8 @@
 class Solution {
-    public boolean containsDuplicate(int[] num) {
-Arrays.sort(num);
-for(int i = 0;i<num.length-1;i++){
-   if(num[i]==num[i+1]){
+    public boolean containsDuplicate(int[] number) {
+Arrays.sort(number);
+for(int i = 0;i<number.length-1;i++){
+   if(number[i]==number[i+1]){
     return true;
    }
 }
