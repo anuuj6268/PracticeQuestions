1244d01b9d81b67731f3aca574e87c91bbae813c
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-02-15 23:04:27 +0530

    Time: 11 ms (7.84%), Space: 44.3 MB (21.16%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,8 +1,8 @@
 import java.math.BigInteger;
 class Solution {
-    public String multiply(String num, String num1) {
+    public String multiply(String num, String num2) {
         BigInteger a = new BigInteger(num);
-        BigInteger b = new BigInteger(num1);
+        BigInteger b = new BigInteger(num2);
         BigInteger c = a.multiply(b);
         String str1 = c.toString();
         return str1;
