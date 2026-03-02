9499eb3a37d6f25e0be0ed435da057d94323e770
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-03-02 21:14:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -3,17 +3,17 @@ import java.util.Arrays;
 class Solution {
     public int maximumGap(int[] nums) {
         Arrays.sort(nums);
-        int numbers = 0; 
+        int number = 0; 
         int j = 0;
     for(int i = 0;i<nums.length-1;i++){
      j = nums[i+1]-nums[i];
-if(numbers<j){
-    numbers = j;
+if(number<j){
+    number = j;
 }
     }
 if(nums.length==1){
     return 0;
 }
-return numbers;
+return number;
     }
 }
