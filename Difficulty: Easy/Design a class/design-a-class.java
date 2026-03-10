be9ae9c3ca7bfbe3513f28ff87f991ab3ce095a8
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-03-10 16:53:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Design a class/design-a-class.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+
+// Design a class as described in the question
+// Code here
+class MyClass{
+    public void display(){
+        System.out.println("Hello World");
+    }
+}
+
+class OOPS{
+    public static void main(String args[]){
+        MyClass c1 = new MyClass();
+        c1.display();
+    }
+}
