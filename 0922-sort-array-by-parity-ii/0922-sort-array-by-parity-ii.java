615d3cbfcd3241de68b8bdae559af49b258179e3
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-03-29 23:52:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int[] sortArrayByParityII(int[] numss) {
-        int[] arr = new int[numss.length];
+    public int[] sortArrayByParityII(int[] nums) {
+        int[] arr = new int[nums.length];
         int evenIndex = 0;
         int oddIndex = 1;
-    for(int i = 0;i<numss.length;i++){
-        if(numss[i]%2==0){
-            arr[evenIndex] = numss[i];
+    for(int i = 0;i<nums.length;i++){
+        if(nums[i]%2==0){
+            arr[evenIndex] = nums[i];
             evenIndex +=2;
         }
     }
 
-    for(int i = 0;i<numss.length;i++){
-        if(numss[i]%2!=0){
-            arr[oddIndex] = numss[i];
+    for(int i = 0;i<nums.length;i++){
+        if(nums[i]%2!=0){
+            arr[oddIndex] = nums[i];
             oddIndex +=2;
         }
     }
