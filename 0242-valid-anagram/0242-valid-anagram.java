f751449d1c5063ca526fbde5ebf6cc9be3c0bafd
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-04-03 19:47:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        TreeMap<Character,Integer> map = new TreeMap<>();
-        TreeMap<Character,Integer> map2 = new TreeMap<>();
+        HashMap<Character,Integer> map = new HashMap<>();
+        if(s.length()!=t.length()){
+            return false;
+        }
         for(int i = 0;i<s.length();i++){
             char ch = s.charAt(i);
             if(map.containsKey(ch)){
@@ -11,16 +13,18 @@ class Solution {
                 map.put(ch,1);
             }
         }
-        for(int i = 0;i<t.length();i++){
+        for(int i=0;i<t.length();i++){
             char ch = t.charAt(i);
-            if(map2.containsKey(ch)){
-                map2.put(ch,map2.get(ch)+1);
-            }
-            else{
-                map2.put(ch,1);
+            if(map.get(ch)!=null){
+                if(map.get(ch)==1){
+                    map.remove(ch);
+                }
+                else{
+                    map.put(ch,map.get(ch)-1);
+                }
             }
         }
-        if(map.equals(map2)){
+        if(map.isEmpty()){
             return true;
         }
         return false;
