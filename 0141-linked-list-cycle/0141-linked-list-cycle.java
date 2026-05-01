262a5e58899beb44c012799ec200a51164c4d662
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-05-01 22:25:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,9 +10,9 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode header) {
-        ListNode slow = header;
-        ListNode fast = header;
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
         while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
