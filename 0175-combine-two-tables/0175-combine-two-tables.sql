66f18f6ec46d4aaf6c73c2994392c866b3e93c85
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-06-02 21:46:21 +0530

    Time: 432 ms (38.39%), Space: 0B (100%) - LeetHub - Time: O(?), Space: O(?)

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -2,7 +2,9 @@
 select firstName,lastName,  city ,state from
 Person left join Address
 
-
-
-
 on Person.personId = Address.personId;
+
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
