1eed16e6cc619c57221ae539b8bf4a15de6676c5
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-06-15 21:22:56 +0530

    Time: 1 ms (31.29%), Space: 43.2 MB (33.08%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,14 +1,10 @@
 class Solution {
-public static String[] wordList(String strr){
-    return strr.split(" ");
+public static String[] wordList(String str){
+    return str.split(" ");
 }
-public int lengthOfLastWord(String strr) {
-String wordlist[] = wordList(strr);
+public int lengthOfLastWord(String str) {
+String wordlist[] = wordList(str);
 String lastWords = wordlist[wordlist.length-1];
 return lastWords.length();        
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
