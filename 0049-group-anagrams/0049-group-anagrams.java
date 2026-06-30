8764fd9ca025a602815f54155d30e666a3f36c5e
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-06-30 17:38:46 +0530

    Time: 20 ms (13.22%), Space: 50.1 MB (22.24%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List> map = new HashMap<>();
+        for(String s : strs){
+            int[] arr = new int[26];
+            for(char ch:s.toCharArray()){
+                arr[ch-'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int a : arr){
+                sb.append("#");
+                sb.append(a);
+            }
+            String key = sb.toString();
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<String>());
+            }   
+            map.get(key).add(s);
+        }
+
+return new ArrayList(map.values());
+    }
+}
