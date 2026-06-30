6f9063adbd8b68e5430502823890069a88bdb756
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-06-30 15:50:50 +0530

    Time: 21 ms (8.04%), Space: 47.3 MB (5.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,32 +1,33 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         HashMap<Character,Integer> map = new HashMap<>();
-        if(s.length()!=t.length()){
-            return false;
-        }
+        if(s.length()!=t.length()) return false;
         for(int i = 0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
+            int count = 1;
+            if(!map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),count);
             }
             else{
-                map.put(ch,1);
+                count = map.get(s.charAt(i));
+                map.put(s.charAt(i),count+1);
             }
         }
-        for(int i=0;i<t.length();i++){
-            char ch = t.charAt(i);
-            if(map.get(ch)!=null){
-                if(map.get(ch)==1){
-                    map.remove(ch);
-                }
-                else{
-                    map.put(ch,map.get(ch)-1);
-                }
-            }
-        }
-        if(map.isEmpty()){
-            return true;
-        }
+        for(int i = 0;i<t.length();i++){
+    if (!map.containsKey(t.charAt(i))) {
         return false;
+    }
+           int count = map.get(t.charAt(i))-1;
+           if(count==0){
+            map.remove(t.charAt(i));
+           }
+            else{
+                map.put(t.charAt(i),count);
+            }
+
+        }
+if(map.isEmpty()){
+    return true;
+}
+return false;
     }
 }
