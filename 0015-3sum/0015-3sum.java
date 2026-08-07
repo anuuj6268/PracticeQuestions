d679bb98937b0cc904939a1e45f79e4b7e4c98b6
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-08-07 23:40:37 +0530

    Time: 45 ms (17.73%), Space: 60.3 MB (9.75%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
- List<List<Integer>> list = new ArrayList<>();
- Arrays.sort(nums);
-        for(int i = 0;i<nums.length-2;i++){    
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0;i<nums.length-2;i++){
             int left = i+1;
             int right = nums.length-1;
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
+            int target = -nums[i];
+            if(i>0 && nums[i]==nums[i-1])continue;
+
             while(left<right){
-                if(nums[left]+nums[right]>-(nums[i])){
-                    right--;
-                }
-                else if(nums[left]+nums[right]<-(nums[i])){
-                    left++;
-                }
-                else{
-                    list.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                    left++;
-                    right--;
-                    while(left<right && nums[left]==nums[left-1]){
+                int sum = nums[left]+nums[right];
+                List<Integer> arr = new ArrayList<>();
+                if(sum==target){
+                    arr.add(-target);
+                    arr.add(nums[left]);
+                    arr.add(nums[right]);
+                    list.add(arr);
+                    while(left<right && nums[left]==nums[left+1]){
                         left++;
                     }
-                    while(left<right && nums[right]==nums[right+1]){
+                    while(left<right && nums[right]==nums[right-1]){
                         right--;
                     }
+                    left++;
+                    right--;
+                }
+                else if(sum>target){
+                    right--;
+                }
+                else{
+                    left++;
                 }
             }
+
         }
-        return list;
+
+
+return list;
     }
 }
