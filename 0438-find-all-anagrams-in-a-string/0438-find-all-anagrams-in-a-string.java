853025ae0f4931a42adbbea6d0b26c38ef17f8b3
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-08-15 17:15:34 +0530

    Time: 11 ms (66.75%), Space: 47 MB (46.77%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,28 +1,22 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-                List<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         if(p.length()>s.length()) return list;
 
-
-        int[] sFreq = new int[26];
-        int[] pFreq = new int[26];
+        int[] sF = new int[26];
+        int[] pF = new int[26];
         for(int i = 0;i<p.length();i++){
-            pFreq[p.charAt(i)-'a']++;
-            sFreq[s.charAt(i)-'a']++;
+            pF[p.charAt(i)-'a']++;
+            sF[s.charAt(i)-'a']++;
         }
-        if(Arrays.equals(sFreq, pFreq)){
+        if(Arrays.equals(pF,sF)){
             list.add(0);
         }
 
         for(int i = p.length();i<s.length();i++){
-            sFreq[s.charAt(i)-'a']++;
-            sFreq[s.charAt(i-p.length())-'a']--;
-
-            if(Arrays.equals(sFreq, pFreq)){
+            sF[s.charAt(i)-'a']++;
+            sF[s.charAt(i-p.length())-'a']--;
+            if(Arrays.equals(sF,pF)){
                 list.add(i-p.length()+1);
             }
         }
