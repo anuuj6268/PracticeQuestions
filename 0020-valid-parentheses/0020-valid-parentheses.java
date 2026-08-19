8fe12ae423e929289deaa7683d335ed10124e6aa
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-08-20 00:22:43 +0530

    Time: 4 ms (36.69%), Space: 43.5 MB (25.87%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,24 @@
+import java.util.Deque;
+
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()<2) return false;
-     Deque<Character> stack = new ArrayDeque<>();
-     for(int i = 0;i<s.length();i++){
-        if(s.charAt(i) =='(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
-            stack.push(s.charAt(i));
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+                stack.push(s.charAt(i));
+            }
+            else{
+                if(stack.isEmpty()) return false;
+                
+                if((s.charAt(i)==')' && stack.peek()=='(') || (stack.peek() =='[' && s.charAt(i) ==']') || (stack.peek() =='{' && s.charAt(i)=='}')){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
         }
- else{
-    if(stack.isEmpty()) return false;
-
-    else if((stack.peek() == '(' && s.charAt(i) == ')') || (stack.peek()=='[' && s.charAt(i) ==']') || (stack.peek() == '{' && s.charAt(i) =='}')){
-        stack.pop();
+return stack.isEmpty();
     }
-
-    else{
-        return false;
-    }
- }
-
-    }
-    if(!stack.isEmpty()) return false;
-
-     return true;
-}
 }
