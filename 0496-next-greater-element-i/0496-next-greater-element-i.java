545ae1eeae1fd5aab8284912e73ee30977d53b82
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-08-21 23:33:10 +0530

    Time: 4 ms (74.17%), Space: 45.3 MB (50.25%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int arr[] = new int[nums1.length];
         Deque<Integer> stack = new ArrayDeque<>();
-        int[] arr = new int[nums1.length];
         Map<Integer,Integer> map = new HashMap<>();
-
         for(int i = 0;i<nums2.length;i++){
             while(!stack.isEmpty() && nums2[i]>stack.peek()){
                 map.put(stack.peek(),nums2[i]);
@@ -11,14 +10,14 @@ class Solution {
             }
             stack.push(nums2[i]);
         }
-for(int i = 0;i<nums1.length;i++){
-    if(map.containsKey(nums1[i])){
-        arr[i] = map.get(nums1[i]);
+    for(int i = 0;i<nums1.length;i++){
+        if(map.containsKey(nums1[i])){
+            arr[i] = map.get(nums1[i]);
+        }
+        else{
+            arr[i] = -1;
+        }
     }
-    else{
-        arr[i] = -1;
-    }
-}
 return arr;
     }
 }
