0fcfc307e9475f13d3c8d20a6cf74803100b52be
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-08-22 23:32:12 +0530

    Time: 3 ms (99.83%), Space: 94.2 MB (90.36%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,52 +1,28 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-
-    public ListNode middle(ListNode head){
-   
-
-
-        ListNode slower = head;
-        ListNode fast = head;
-        while(fast!=null && fast.next!=null){
-            slower = slower.next;
-            fast = fast.next.next;
-        }
-        return slower;
-    }
     public boolean isPalindrome(ListNode head) {
-        if(head==null || head.next == null){
-            return true;
-        }
-        ListNode mid = middle(head);
+        ListNode slow = head;
+        ListNode fast = head;
+while(fast!=null && fast.next !=null){
+    slow = slow.next;
+    fast = fast.next.next;
+}
 
-        ListNode prev = null;
-        ListNode curr = mid;
-        ListNode nextt;
-        ListNode left = head;
-        ListNode right;
-       while(curr!=null){
-        nextt = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = nextt;
-       }
-right = prev;
+ListNode prev = null;
+ListNode curr = slow;
 
-while(right!=null){
-    if(left.val != right.val){
-        return false;
-    }
-    left = left.next;
-    right = right.next;
+while(curr!=null){
+ListNode next = curr.next;
+curr.next = prev;
+prev = curr;
+curr = next;
+}
+ListNode first = head;
+ListNode second = prev;
+while(second!=null){
+    if(second.val != first.val) return false;
+
+    second = second.next;
+    first = first.next;
 }
 return true;
     }
