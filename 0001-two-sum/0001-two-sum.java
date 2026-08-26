11d60e07a27425aacd47ea8bcaec72d7dca1b39f
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-08-26 23:59:46 +0530

    Add logic to find two indices for target sum
    Implement logic to find two indices that sum to target.

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,6 +4,9 @@ class Solution {
         for(int i = 0;i<nums.length;i++){
             for(int j = i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
+
+
+                    
                 arr[0] = i;
 
 
