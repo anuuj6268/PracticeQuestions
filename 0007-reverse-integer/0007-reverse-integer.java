3855758a17594d1d8ba28970fc41f54ef3e9bb67
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-08-27 23:59:10 +0530

    Update 0007-reverse-integer.java

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,7 +3,7 @@ class Solution {
     int num = 0;
 
     while(x!=0){
-    num = (num*10)+x%10;
+    num = (nuberm*10)+x%10;
     x = x/10;
          }
          if((num > -2147483648) && (num < 2147483647)){
