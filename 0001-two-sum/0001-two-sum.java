69175539f13c47c167ef5497b796b191764b1ef7
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-09-01 23:41:36 +0530

    Time: 51 ms (5.88%), Space: 47 MB (74.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,8 +4,6 @@ class Solution {
         for(int i = 0;i<num.length;i++){
             for(int j = i+1;j<num.length;j++){
                 if(num[i]+num[j]==target){
-
-                    
                 arr[0] = i;
                 arr[1] = j;
                 }
