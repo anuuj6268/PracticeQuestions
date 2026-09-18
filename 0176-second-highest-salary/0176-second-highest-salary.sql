e8fd22b6e096c5c86ca4840686c2293ace5709b0
Anuj <147028172+anuuj6268@users.noreply.github.com>
2026-09-18 23:02:28 +0530

    Time: 282 ms (79.35%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,1 @@
-select max(salary) as SecondHighestSalary
-from employee where 
-salary<(select max(salary) 
-from employee);
+select max(salary) as SecondHighestSalary from employee where salary<(select max(salary) from employee);
